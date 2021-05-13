bb6b22b0e903fa74678356a557d5c3cd1d12a3da
Ayse Cimen Basar <aysebasar91@gmail.com>
2021-05-13 20:16:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ fuzzyMatch(driverNames,"bobby")
 console.log(fuzzyMatch(driverNames,"bobby"))
 
 const matchName = (arr, str) => {
-    return arr.filter(driverName => driverName.name === name )
+    return arr.filter(drivers => driverName.name === str )
 };
 
