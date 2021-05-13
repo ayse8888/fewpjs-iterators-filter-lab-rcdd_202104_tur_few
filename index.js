cc670f86e3cfc79409352a5f9fbd3d09236c0f8b
Ayse Cimen Basar <aysebasar91@gmail.com>
2021-05-13 20:12:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 // Code your solution here
 
 const driverNames = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "bobby"]
-// const findMatching = (arr, str) => {
-//   return arr.filter(drivers => drivers.toLowerCase() === str.toLowerCase())
-// }
-// console.log(findMatching(driverNames))
-// findMatching(driverNames)
+const findMatching = (arr, str) => {
+  return arr.filter(drivers => drivers.toLowerCase() === str.toLowerCase())
+}
+console.log(findMatching(driverNames))
+findMatching(driverNames)
 
-// const fuzzyMatch = (arr, str) => {
-//   const result = arr.filter(drivers => drivers).map(item => item[0])
-//   console.log(result === str.split(" ")[0])
-//   return result
-// }
-// console.log(fuzzyMatch(driverNames))
 
 function fuzzyMatch(array, name){    return array.filter(driverName => {return driverName[0] === name[0] })
 };
